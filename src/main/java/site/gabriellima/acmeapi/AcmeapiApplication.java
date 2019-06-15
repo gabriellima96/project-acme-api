925ce2d85335b2288e12f8ef78b4ac0129806078
Gabriel Lima <gabriellima.silva96@gmail.com>
2019-06-15 15:10:50 -0300

    Projeto inicial

---
+++ src/main/java/site/gabriellima/acmeapi/AcmeapiApplication.java
@@ -0,0 +1,13 @@
+package site.gabriellima.acmeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeapiApplication.class, args);
+    }
+
+}
